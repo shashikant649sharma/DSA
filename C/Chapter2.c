dec1b18d6a870f5427fc837857f3e95650cc8da1
Shashikant Sharma <76623693+shashikant649sharma@users.noreply.github.com>
2024-12-05 11:33:17 +0000

    chapter2

---
+++ C/Chapter2.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+// type declaration instruction 
+// int a = 16, b , c ;
+
+// c = 5 ; 
+// b = 45;
+// printf("The square of %d is %d\n", a, c);
+
+//arithmatic instruction
+// pow (b,c) = 45^5 the power operator
+// int d = b + c;
+// int e = b - c;
+// int f = b * c;
+// int g = b / c;
+// int h = b % c;
+// // double p = pow(b,c);
+
+// double p = pow(5,2);
+// printf("The value of p is %f\n", p);
+
+// // modulus operator
+// int m =  9% 5;
+// printf("The value of m is %d\n", m);
+// return 0;
+
+
+// int a = 3; 
+
+// if(a%2 == 0){
+//     printf("The number is even\n");
+// }
+// else{
+//     printf("The number is odd\n");
+
+// }
+
+
+// int a = 1.999999;
+
+// printf("The value of a is %d\n", a);
+
+
+// control instruction
+
+
+return 0;
+}
